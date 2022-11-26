a4bc562ed28731052e207e6011e060432817032f
Steven Ritchie <stevensmex05@gmail.com>
2022-11-26 12:13:35 +0100

    Update

--- scripts/NetworkSetup.gd
+++ scripts/NetworkSetup.gd
@@ -10,6 +10,8 @@ func _on_IpAddress_text_changed(new_text):
 func _on_Host_pressed():
 	Network.create_server()
 	hide()
+	
+	Global.emit_signal("instance_player",get_tree().get_network_unique_id())
 
 func _on_Join_pressed():
 	Network.join_server()
