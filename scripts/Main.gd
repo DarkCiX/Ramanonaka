6308e1c37fb0c58ab59329643f3a5c95078f1604
Steven Ritchie <stevensmex05@gmail.com>
2022-11-27 14:46:05 +0300

    mety io

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,16 +6,13 @@ func _ready():
 	get_tree().connect("network_peer_connected", self , "_player_connected")
 	get_tree().connect("network_peer_disconnected", self , "_player_disconnected")
 	Global.connect("instance_player", self , "_instance_player")
-	rpc_config("_instance_player",MultiplayerAPI.RPC_MODE_REMOTE)
+	
 
 	if get_tree().network_peer != null:
 		Global.emit_signal("toggle_network_setup",false)
 
 remote func _instance_player(id):
-	if get_tree().is_network_server():
-		for p in Network.players:
-			rpc_id(id,"_instance_player",p)
-	Network.players[id] = id;
+	
 	var player_instance = player.instance()
 	player_instance.set_network_master(id)
 	player_instance.name = str(id)
@@ -23,8 +20,7 @@ remote func _instance_player(id):
 	player_instance.global_transform.origin = Vector3(0,15,0)
 
 func _player_connected(id):
-	print("Player" + str(get_tree().get_network_unique_id()) + "connected")
-	print(get_tree().get_network_unique_id())
-	rpc("_instance_player",get_tree().get_network_unique_id())
+	print("Player" + str(id) + "connected")
+	_instance_player(id)
 	
 
--- scripts/NetworkSetup.gd
+++ scripts/NetworkSetup.gd
@@ -16,6 +16,8 @@ func _on_Host_pressed():
 func _on_Join_pressed():
 	Network.join_server()
 	hide()
-
+	
+	Global.emit_signal("instance_player",get_tree().get_network_unique_id())
+	
 func _toggle_network_setup(visible_toggle):
 	visible = visible_toggle
